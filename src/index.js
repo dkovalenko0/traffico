2c3e669d51c84177b8cf01b9e4de8f3f1501b8c1
dkovalenko0 <kovalenkodima715@gmail.com>
2020-04-10 18:43:29 +0300

    create FAQ block

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-import './styles/style.scss'
+import "./styles/style.scss";
+
+const li = document.querySelectorAll(".question");
+// const hiddenBlock = document.querySelector(".answer");
+
+li.forEach((el) => {
+  el.onclick = function () {
+    this.classList.toggle("active");
+    // this.classList.toggle("showed-block");
+  };
+});
