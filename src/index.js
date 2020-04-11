23acb85f7f2d1cb3fbd01f855c37ce94ab4a7e3d
dkovalenko0 <kovalenkodima715@gmail.com>
2020-04-11 15:02:07 +0300

    create clients block

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import "./styles/style.scss";
+import $ from "jquery";
+import "slick-carousel";
 
 const li = document.querySelectorAll(".question");
 // const hiddenBlock = document.querySelector(".answer");
@@ -8,4 +10,13 @@ li.forEach((el) => {
     this.classList.toggle("active");
     // this.classList.toggle("showed-block");
   };
+});
+
+$(".slider").slick({
+  infinite: true,
+  speed: 300,
+  slidesToShow: 2,
+  centerMode: true,
+  nextArrow: `<img src="./images/slider-arrow.svg" alt="arrow" class="arrows next-arrow" />`,
+  prevArrow: `<img src="./images/slider-arrow.svg" alt="arrow" class="arrows prev-arrow" />`,
 });
